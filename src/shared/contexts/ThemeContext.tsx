c56080510085b84abbf8a3eb4aab5abd9894c5bb
Fabio Nascimento <fabiogtr@hotmail.com>
2024-01-26 15:32:36 -0300

    Criado menu lateral e configurado responsividade

--- src/shared/contexts/ThemeContext.tsx
+++ src/shared/contexts/ThemeContext.tsx
@@ -1,7 +1,6 @@
-import { createContext, useCallback, useContext, useMemo, useState } from "react";
-import { Box, ThemeProvider } from "@mui/material";
-
-import { DarkTheme, LightTheme } from "../themes";
+import { createContext, useCallback, useContext, useMemo, useState } from 'react';
+import { Box, ThemeProvider } from '@mui/material';
+import { DarkTheme, LightTheme } from '../themes';
 
 interface IThemeContextData {
   themeName: 'light' | 'dark';
@@ -16,20 +15,20 @@ const ThemeContext = createContext({} as IThemeContextData);
 
 export const useAppThemeContext = () => {
   return useContext(ThemeContext);
-}
+};
 
 export const AppThemeProvider: React.FC<IThemeProviderProps> = ({ children }) => {
   const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
 
   const toggleTheme = useCallback(() => {
-    setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light')
+    setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
   }, []);
 
   const theme = useMemo(() => {
     if (themeName === 'light') return LightTheme;
 
     return DarkTheme;
-  }, [themeName])
+  }, [themeName]);
 
   return (
     <ThemeContext.Provider value={{ themeName, toggleTheme }}>
@@ -39,5 +38,5 @@ export const AppThemeProvider: React.FC<IThemeProviderProps> = ({ children }) =>
         </Box>
       </ThemeProvider>
     </ThemeContext.Provider>
-  )
-}
+  );
+};

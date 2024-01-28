6f1b955ee458e53dbef40264de4bc9914e585945
Fabio Nascimento <fabiogtr@hotmail.com>
2024-01-28 16:29:16 -0300

    Ferramentas de detalhe

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { FerramentasDaListagem } from "../../shared/components";
+import { FerramentasDeDetalhe } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 
 export const Dashboard = () => {
@@ -6,10 +6,7 @@ export const Dashboard = () => {
     <LayoutBaseDePagina 
       titulo='Página Inicial' 
       barraDeFerramentas={(
-        <FerramentasDaListagem 
-          mostrarInputBusca
-          textoBotaoNovo="Nova"
-        />
+        <FerramentasDeDetalhe />
       )}
     >
       Testando
